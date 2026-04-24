b703f8ad675204877cedc3917ae1f006fe6bde6f
Boqian Liu <116846325+BotsianLio@users.noreply.github.com>
2026-04-24 08:47:56 -0600

    Enhance main function for camera and video selection
    Refactor main function to handle video selection and window launching.

--- GhostStream/src/main.py
+++ GhostStream/src/main.py
@@ -2,19 +2,25 @@ import sys
 from PyQt5.QtWidgets import QApplication, QDialog
 from gui.app_window import AppWindow
 from gui.selector import CameraSelector
+from gui.video_window import VideoWindow
 
 def main():
     app = QApplication(sys.argv)
     
-    # 1. Launch Selector First
     selector = CameraSelector()
-    
+    selector.show()
+    sys.exit(app.exec_())
     if selector.exec_() == QDialog.Accepted:
-        selected_index = selector.selected_index
-        
-        # 2. Pass index to AppWindow (It will handle the Thread/Worker)
-        window = AppWindow(selected_index)
-        window.show()
+        if selector.rval == "Camera": 
+            selected_index = selector.selected_index
+
+            window = AppWindow(selected_index)
+            window.show()
+        elif selector.rval == "Video": 
+            video_path = selector.filepath
+
+            window = VideoWindow(video_path)
+            window.show()
         
         sys.exit(app.exec_())
     else:
